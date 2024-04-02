d1513e30bb28b17ed254cc8c6c99d790cca2786f
Reis <samuelreisesilva06@gmail.com>
2024-04-02 17:05:56 -0400

    adicionado exemplo subclasse Diretor

--- Pessoa.cs
+++ Pessoa.cs
@@ -52,3 +52,13 @@ public class Funcionario : Pessoa {
         set { salario = value; }
     }
 }
+
+public class Diretor : Funcionario {
+     public Diretor(string nome, int idade, string cpf, double salario) : base(nome, idade, cpf, salario) {
+
+     }
+
+     public override double Bonus {
+        return base.Bonus() + 1000;
+     }
+}
